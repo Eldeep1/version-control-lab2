ac1c74b00d7c1681a54e956c32c69576334c6bf1
Eldeep1 <alieldeep111@gmail.com>
2025-12-30 11:24:19 +0200

    initial commit

---
+++ HelloITI.java
@@ -0,0 +1,13 @@
+public class HelloITI {
+
+    public static void main(String[] args) {
+        printHelloITI();
+        printHelloGit();
+    }
+        private static void printHelloITI(){
+        System.out.println("Hello ITI");
+    }
+    private static void printHelloGit(){
+        System.out.println("Hello Git");
+    }
+}
